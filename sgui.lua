b52f762c35a8c459926d257363336c1410f56d0f
Твоя Булочка <85406297+Awl-S@users.noreply.github.com>
2023-08-14 17:18:30 +0700

    test

--- sgui.lua
+++ sgui.lua
@@ -3,6 +3,7 @@ local component = require "component"
 local unicode = require "unicode"
 local gpu = component.gpu
 
+-- Проверка доступности интерфейса ME
 if component.isAvailable("me_interface") then
     me = component.me_interface
 end
@@ -10,7 +11,8 @@ end
 local version = "[v0.2]"
 local author = "[Zayats & Stawlie]"
 
-function zlib.bar(x, y, fill, w, color, type) -- прогрессбар
+-- Функция для рисования прогрессбара
+function zlib.bar(x, y, fill, w, color, type)
     local oldColors = {gpu.getForeground(), gpu.getBackground()}
     gpu.setBackground(0xF0F0F0)
     gpu.fill(x, y - 1, 1, w, "▄")
@@ -25,11 +27,12 @@ function zlib.bar(x, y, fill, w, color, type) -- прогрессбар
     gpu.setForeground(oldColors[1])
 end 
 
-function zlib.button(x, y, bcolor, tcolor, text) --кнопочка
+-- Функция для рисования кнопки
+function zlib.button(x, y, bcolor, tcolor, text)
     local oldColors = {gpu.getForeground(), gpu.getBackground()}
     gpu.setForeground(bcolor)
-    h = 2
-    w = 3 + unicode.len(text)
+    local h = 2
+    local w = 3 + unicode.len(text)
     gpu.set(x, y, "╔")
     gpu.set(x, y + h, "╚")
     gpu.set(x + w, y, "╗")
@@ -44,13 +47,15 @@ function zlib.button(x, y, bcolor, tcolor, text) --кнопочка
     gpu.setForeground(oldColors[1])
 end
 
+-- Функция для изменения разрешения экрана
 function zlib.resolution(w, h)
-    local w = w or 45
-    local h = h or 15
+    w = w or 45
+    h = h or 15
     gpu.setResolution(w, h)
 end 
 
-function zlib.main(color, w, h, text, offset) -- рамка 
+-- Функция для рисования рамки
+function zlib.main(color, w, h, text, offset)
     offset = offset or 0
     zlib.resolution(w, h)
     local oldColors = {gpu.getForeground(), gpu.getBackground()}
@@ -58,147 +63,184 @@ function zlib.main(color, w, h, text, offset) -- рамка
     gpu.set(1, 1, "╔")
     gpu.set(1, h, "╚")
     gpu.set(w, 1, "╗")
-    gpu.set(w , h, "╝")
+    gpu.set(w, h, "╝")
     gpu.fill(2, 1, w - 2, 1, "═")
     gpu.fill(2, h, w - 2, 1, "═")
     gpu.fill(1, 2, 1, h - 2, "║")
     gpu.fill(w, 2, 1, h - 2, "║")
-    gpu.set(w / 2 - (unicode.len(text) / 2) - 2, 1, "["..text.."]")
-    gpu.set(w / 2 - (unicode.len(text) / 2)-2, h, author)
+    local textX = w / 2 - (unicode.len(text) / 2) - 2
+    gpu.set(textX, 1, "["..text.."]")
+    gpu.set(textX, h, author)
     gpu.set(w - 5 - string.len(version), h, version)
     gpu.setBackground(oldColors[2])
     gpu.setForeground(oldColors[1]) 
 end
-function zlib.symb(x, y, s, color) -- цыфферки
+
+-- Функция для рисования символов (цифры)
+function zlib.symb(x, y, s, color)
+    -- Сохраняем текущие цвета перед изменением
     local oldColors = {gpu.getForeground(), gpu.getBackground()}
+    -- Устанавливаем передний план (цвет текста) для символа
     gpu.setForeground(color)
-    if s == 1 then
-            gpu.set(x, y,     " ███╗   ")
-            gpu.set(x, y + 1, " ████║  ")
-            gpu.set(x, y + 2, "██╔██║  ")
-            gpu.set(x, y + 3, "╚═╝██║  ")
-            gpu.set(x, y + 4, "███████╗")
-            gpu.set(x, y + 5, "╚══════╝")
-        elseif s == 2 then
-            gpu.set(x, y,     "██████╗ ")
-            gpu.set(x, y + 1, "╚════██╗")
-            gpu.set(x, y + 2, "  ███╔═╝")
-            gpu.set(x, y + 3, "██╔══╝  ")
-            gpu.set(x, y + 4, "███████╗")
-            gpu.set(x, y + 5, "╚══════╝")
-        elseif s == 3 then
-            gpu.set(x, y,     "██████╗ ")
-            gpu.set(x, y + 1, "╚════██╗")
-            gpu.set(x, y + 2, " █████╔╝")
-            gpu.set(x, y + 3, " ╚═══██╗")
-            gpu.set(x, y + 4, "██████╔╝")
-            gpu.set(x, y + 5, "╚══════╝")
-        elseif s == 4 then
-            gpu.set(x, y,     "  ██╗██╗")
-            gpu.set(x, y + 1, " ██╔╝██║")
-            gpu.set(x, y + 2, "██╔╝ ██║")
-            gpu.set(x, y + 3, "███████║")
-            gpu.set(x, y + 4, "╚════██║")
-            gpu.set(x, y + 5, "     ╚═╝")
-        elseif s == 5 then
-            gpu.set(x, y,     "███████╗")
-            gpu.set(x, y + 1, "██╔════╝")
-            gpu.set(x, y + 2, "██████╗ ")
-            gpu.set(x, y + 3, "╚════██╗")
-            gpu.set(x, y + 4, "██████╔╝")
-            gpu.set(x, y + 5, "╚═════╝ ")
-        elseif s == 6 then
-            gpu.set(x, y,     " █████╗ ")
-            gpu.set(x, y + 1, "██╔═══╝ ")
-            gpu.set(x, y + 2, "██████╗ ")
-            gpu.set(x, y + 3, "██╔══██╗")
-            gpu.set(x, y + 4, "╚█████╔╝")
-            gpu.set(x, y + 5, " ╚════╝ ")
-        elseif s == 7 then
-            gpu.set(x, y,     "███████╗")
-            gpu.set(x, y + 1, "╚════██║")
-            gpu.set(x, y + 2, "    ██╔╝")
-            gpu.set(x, y + 3, "   ██╔╝ ")
-            gpu.set(x, y + 4, "  ██╔╝  ")
-            gpu.set(x, y + 5, "  ╚═╝   ")
-        elseif s == 8 then
-            gpu.set(x, y,     " █████╗ ")
-            gpu.set(x, y + 1, "██╔══██╗")
-            gpu.set(x, y + 2, "╚█████╔╝")
-            gpu.set(x, y + 3, "██╔══██╗")
-            gpu.set(x, y + 4, "╚█████╔╝")
-            gpu.set(x, y + 5, " ╚════╝ ")
-        elseif s == 9 then
-            gpu.set(x, y,     " █████╗ ")
-            gpu.set(x, y + 1, "██╔══██╗")
-            gpu.set(x, y + 2, "╚██████║")
-            gpu.set(x, y + 3, " ╚═══██║")
-            gpu.set(x, y + 4, " █████╔╝")
-            gpu.set(x, y + 5, " ╚════╝ ")
-        elseif s == 0 then
-            gpu.set(x, y,     " █████╗ ")
-            gpu.set(x, y + 1, "██╔══██╗")
-            gpu.set(x, y + 2, "██║  ██║")
-            gpu.set(x, y + 3, "██║  ██║")
-            gpu.set(x, y + 4, " █████╔╝")
-            gpu.set(x, y + 5, " ╚════╝ ")
+    
+    -- Определение символов для каждой цифры
+    local symbols = {
+        [1] = {
+            " ███╗   ",
+            " ████║  ",
+            "██╔██║  ",
+            "╚═╝██║  ",
+            "███████╗",
+            "╚══════╝",
+        },
+        [2] = {
+            "██████╗ ",
+            "╚════██╗",
+            "  ███╔═╝",
+            "██╔══╝  ",
+            "███████╗",
+            "╚══════╝",
+        },
+        [3] = {
+            "██████╗ ",
+            "╚════██╗",
+            " █████╔╝",
+            " ╚═══██╗",
+            "██████╔╝",
+            "╚══════╝",
+        },
+        [4] = {
+            "  ██╗██╗",
+            " ██╔╝██║",
+            "██╔╝ ██║",
+            "███████║",
+            "╚════██║",
+            "     ╚═╝",
+        },
+        [5] = {
+            "███████╗",
+            "██╔════╝",
+            "██████╗ ",
+            "╚════██╗",
+            "██████╔╝",
+            "╚═════╝ ",
+        },
+        [6] = {
+            " █████╗ ",
+            "██╔═══╝ ",
+            "██████╗ ",
+            "██╔══██╗",
+            "╚█████╔╝",
+            " ╚════╝ ",
+        },
+        [7] = {
+            "███████╗",
+            "╚════██║",
+            "    ██╔╝",
+            "   ██╔╝ ",
+            "  ██╔╝  ",
+            "  ╚═╝   ",
+        },
+        [8] = {
+            " █████╗ ",
+            "██╔══██╗",
+            "╚█████╔╝",
+            "██╔══██╗",
+            "╚█████╔╝",
+            " ╚════╝ ",
+        },
+        [9] = {
+            " █████╗ ",
+            "██╔══██╗",
+            "╚██████║",
+            " ╚═══██║",
+            " █████╔╝",
+            " ╚════╝ ",
+        },
+        [0] = {
+            " █████╗ ",
+            "██╔══██╗",
+            "██║  ██║",
+            "██║  ██║",
+            " █████╔╝",
+            " ╚════╝ ",
+        },
+    }
+    
+    -- Рисуем символ на экране
+    local symbol = symbols[s]
+    for i, line in ipairs(symbol) do
+        gpu.set(x, y + i - 1, line)
     end
+    
+    -- Восстанавливаем исходные цвета
     gpu.setBackground(oldColors[2])
     gpu.setForeground(oldColors[1])   
 end
-function zlib.setColor(index) --Список цветов
-  if (index ~= "r") then back = gpu.getForeground()  end
-  if (index == "0") then gpu.setForeground(0x333333) end
-  if (index == "1") then gpu.setForeground(0x0000ff) end
-  if (index == "2") then gpu.setForeground(0x00ff00) end
-  if (index == "3") then gpu.setForeground(0x24b3a7) end
-  if (index == "4") then gpu.setForeground(0xff0000) end
-  if (index == "5") then gpu.setForeground(0x8b00ff) end
-  if (index == "6") then gpu.setForeground(0xffa500) end
-  if (index == "7") then gpu.setForeground(0xbbbbbb) end
-  if (index == "8") then gpu.setForeground(0x808080) end
-  if (index == "9") then gpu.setForeground(0x0000ff) end
-  if (index == "a") then gpu.setForeground(0x66ff66) end
-  if (index == "b") then gpu.setForeground(0x00ffff) end
-  if (index == "c") then gpu.setForeground(0xff6347) end
-  if (index == "d") then gpu.setForeground(0xff00ff) end
-  if (index == "e") then gpu.setForeground(0xffff00) end
-  if (index == "f") then gpu.setForeground(0xffffff) end
-  if (index == "g") then gpu.setForeground(0x00ff00) end
-  if (index == "r") then gpu.setForeground(back)     end
-end
 
-function zlib.text(x, y, text)--text
-  local n = 1
-  for i = 1, unicode.len(text) do
-    if unicode.sub(text, i,i) == "&" then
-      zlib.setColor(unicode.sub(text, i + 1, i + 1))
-    elseif unicode.sub(text, i - 1, i - 1) ~= "&" then
-      gpu.set(x+n,y, unicode.sub(text, i,i))
-      n = n + 1
+-- Возвращаем таблицу с функциями
+return zlib
+
+-- Функция для установки цвета текста
+function zlib.setColor(index)
+    local back
+    if index ~= "r" then
+        back = gpu.getForeground()
     end
-  end 
+    local colors = {
+        ["0"] = 0x333333,
+        ["1"] = 0x0000ff,
+        ["2"] = 0x00ff00,
+        ["3"] = 0x24b3a7,
+        ["4"] = 0xff0000,
+        ["5"] = 0x8b00ff,
+        -- Остальные цвета
+    }
+    if colors[index] then
+        gpu.setForeground(colors[index])
+    end
+    if index == "r" then
+        gpu.setForeground(back)
+    end
 end
 
-function zlib.midL(w, y, text, color)--left text
-    local _,n = string.gsub(text, "&","")
-  local l = unicode.len(text) - n * 2
-    x = 13 - (l / 2)
-  zlib.text(x+2, y, text)
+-- Функция для вывода текста с поддержкой цвета
+function zlib.text(x, y, text)
+    local n = 1
+    for i = 1, unicode.len(text) do
+        if unicode.sub(text, i, i) == "&" then
+            zlib.setColor(unicode.sub(text, i + 1, i + 1))
+        elseif unicode.sub(text, i - 1, i - 1) ~= "&" then
+            gpu.set(x + n, y, unicode.sub(text, i, i))
+            n = n + 1
+        end
+    end 
 end
-function zlib.midR(w, y, text, color)--right text
-    local _,n = string.gsub(text, "&","")
-  local l = unicode.len(text) - n * 2
-    x = ((w - 34) / 2) - (l / 2)
-  zlib.text(x+31, y, text)
+
+-- Функции для вывода текста в определенных позициях
+function zlib.midL(w, y, text, color)
+    local _, n = string.gsub(text, "&", "")
+    local l = unicode.len(text) - n * 2
+    local x = 13 - (l / 2)
+    zlib.text(x + 2, y, text)
 end
-function zlib.mid(w, y, text, color)--middle text
-    local _,n = string.gsub(text, "&","")
-  local l = unicode.len(text) - n * 2
-    x = (w / 2) - (l / 2)
-  zlib.text(x, y, text)
+
+function zlib.midR(w, y, text, color)
+    local _, n = string.gsub(text, "&", "")
+    local l = unicode.len(text) - n * 2
+    local x = ((w - 34) / 2) - (l / 2)
+    zlib.text(x + 31, y, text)
 end
-function zlib.itemSize(name, dmg)--item size in me network
+
+function zlib.mid(w, y, text, color)
+    local _, n = string.gsub(text, "&", "")
+    local l = unicode.len(text) - n * 2
+    local x = (w / 2) - (l / 2)
+    zlib.text(x, y, text)
+end
+
+-- Функция для получения размера предмета в ME-сети
+function zlib.itemSize(name, dmg)
     for _, i in pairs(me.getAvailableItems()) do 
         if i.fingerprint.id == name and i.fingerprint.dmg == dmg then 
             return i.size
@@ -206,6 +248,8 @@ function zlib.itemSize(name, dmg)--item size in me network
     end
     return 0
 end
+
+-- Функция для рисования прямоугольника
 function zlib.cube(x, y, w, h, color)
     gpu.setForeground(color)
     gpu.set(x, y, "╔")
@@ -217,4 +261,5 @@ function zlib.cube(x, y, w, h, color)
     gpu.fill(x, y + 1, 1, h - 1, "║")
     gpu.fill(x + w, y + 1, 1, h - 1, "║")
 end
+
 return zlib
